9112c600bc9e4b5bebfb551e802f1eb530b8b28b
peitianx <peitian.xiong@berkeley.edu>
2016-09-19 01:40:26 -0700

    finish hw1

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,10 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    a.map! { |x| x.to_i + 2}
+    a.keep_if { |y| y % 2 == 0 && y <= 10}
+    a.uniq
+    a.inject { |sum, n| sum + n}
   end
 
   def foo(b)
--- hw1.rb
+++ hw1.rb
@@ -1,17 +1,22 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  # (a + b) ** 2
   (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-  a.sort!.map do |b|
-    b + 1
-  end
+  # a.sort!.map do |b|
+  #   b + 1
+  a.sort!.map{ |b| b + 1}
+
 end
+
+# end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  # "#{first_name} #{last_name}"
   "#{first_name} #{last_name}"
 end
 
